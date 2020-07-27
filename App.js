79cf9c410dda0e45aa1607a88980879e39da8e03
Heedong Kim <ruthetum@gmail.com>
2020-07-27 20:15:12 +0900

    Input 카드 UI 만들기

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar, 
+  TextInput, 
+  Dimensions, 
+  Platform, 
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const { height, width } = Dimensions.get("window");
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>To Do List</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"}/>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f23657',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color : "white",
+    fontSize: 30,
+    marginTop: 50,
+    marginBottom: 30,
+    fontWeight: "100"
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width-25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    // 특정 효과를 줄 때 target flatform마다 명칭이 다름 -> platform select를 통해 설정
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
   },
 });
