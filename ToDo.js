2629f98dfc86b93a6f69e20bdacff60fce94a36c
Heedong Kim <ruthetum@gmail.com>
2020-07-27 20:34:28 +0900

    스크롤 뷰 추가

---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+export default class ToDO extends React.Component{
+    render() {
+        return (
+            <View>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
